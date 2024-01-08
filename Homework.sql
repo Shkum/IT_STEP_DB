4ec413f5168ea6c46f27bf3e6fa3d41c7b25bf0a
Sergiy <shkum79@ukr.net>
2024-01-08 23:58:43 +0100

    Homework_04 [200~Створіть однотабличну базу даних «Овочі та фрукти»,

--- Homework.sql
+++ Homework.sql
@@ -1,4 +1,22 @@
 -- Homework 04
 
-CREATE DATABASE Vegetables_and_fruits;
+CREATE TABLE tab(
+	item_name VARCHAR(30),
+	item_type VARCHAR(20),
+	item_color VARCHAR(20),
+	calory INT,
+	description VARCHAR(100)
+	);
+INSERT INTO tab (item_name, item_type, item_color, calory, description)
+VALUES
+    ('Apple', 'fruit', 'red', 52, 'Juicy red apple'),
+    ('Banana', 'fruit', 'yellow', 105, 'Sweet yellow banana'),
+    ('Orange', 'fruit', 'orange', 43, 'Juicy orange'),
+    ('Carrot', 'vegetable', 'orange', 41, 'Sweet carrot'),
+    ('Tomato', 'vegetable', 'red', 18, 'Juicy red tomato'),
+    ('Cucumber', 'vegetable', 'green', 16, 'Fresh green cucumber'),
+    ('Pear', 'fruit', 'green', 57, 'Juicy green pear'),
+    ('Potato', 'vegetable', 'brown', 77, 'Potato with skin'),
+    ('Plum', 'fruit', 'purple', 82, 'Sweet purple plum'),
+    ('Broccoli', 'vegetable', 'green', 55, 'Green broccoli');
 
