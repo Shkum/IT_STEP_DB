dc710034ea63d277412cf30ab4555cf58795d8cb
Sergiy <shkum79@ukr.net>
2024-01-16 18:21:41 +0100

    Вивести назви груп та їх рейтинги з уточненнями до назв полів відповідно до назви таблиці

---
+++ 16.01.24/homework/Homework-02.sql
@@ -0,0 +1,6 @@
+-- Завдання 2 - 02
+-- Вивести назви груп та їх рейтинги з уточненнями до назв полів відповідно до назви таблиці
+
+SELECT name AS "Group Name", rating AS "Group Rating" 
+FROM groups;
+
